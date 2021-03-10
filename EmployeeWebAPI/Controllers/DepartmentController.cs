6192c8566f9be35e5dbb5013ac112b164601b7bc
Tanner Quesenberry <tanner.quesenberry@gmail.com>
2021-03-09 20:44:39 -0800

    Created Department API controller with CRUD methods.

--- EmployeeWebAPI/Controllers/DepartmentController.cs
+++ EmployeeWebAPI/Controllers/DepartmentController.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
 using System.Data;
+using EmployeeWebAPI.Models;
 
 namespace EmployeeWebAPI.Controllers
 {
@@ -51,5 +52,104 @@ namespace EmployeeWebAPI.Controllers
             // Create and return Json from the table data
             return new JsonResult(table);
         }
+
+
+        [HttpPost]
+        public JsonResult Post(Department dep)
+        {
+            //SQL query, table to store returned data, connection string to desired db
+            string query = @"insert into dbo.Department values ('" + dep.Name + @"')";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
+
+            // Create reader to get data, create and open a connection to the db
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+
+                // Create the T-SQL command
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    // Execute the command, load data into table
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+
+                    // Close connections
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult("Added Sucessfully");
+        }
+
+        [HttpPut]
+        public JsonResult Put(Department dep)
+        {
+            //SQL query, table to store returned data, connection string to desired db
+            string query = @"
+                            update dbo.Department set
+                            Name = '" + dep.Name + @"' 
+                            where Id = " + dep.Id + @"";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
+
+            // Create reader to get data, create and open a connection to the db
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+
+                // Create the T-SQL command
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    // Execute the command, load data into table
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+
+                    // Close connections
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult("Updated Sucessfully");
+        }
+
+        // Add what is sent to root parameter
+        [HttpDelete("{id}")]
+        public JsonResult Delete(int id)
+        {
+            //SQL query, table to store returned data, connection string to desired db
+            string query = @"
+                            delete from dbo.Department 
+                            where Id = " + id + @"";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
+
+            // Create reader to get data, create and open a connection to the db
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+
+                // Create the T-SQL command
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    // Execute the command, load data into table
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+
+                    // Close connections
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult("Deleted Sucessfully");
+        }
+
+
     }
 }
--- EmployeeWebAPI/Startup.cs
+++ EmployeeWebAPI/Startup.cs
@@ -42,6 +42,7 @@ namespace EmployeeWebAPI
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            // CORS
             app.UseCors(options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 
             if (env.IsDevelopment())
